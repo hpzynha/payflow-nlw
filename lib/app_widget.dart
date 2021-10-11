ed45eaa608f87df0db93a5dda8a12ff71b54b0fd
hpzynha <larissa.nogueira.rocha@gmail.com>
2021-10-10 20:41:31 -0400

    first commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto_page.dart';
+import 'modules/home/home_page.dart';
+import 'modules/login/login_page.dart';
+import 'modules/splash/splash_page.dart';
+import 'shared/themes/app_colors.dart';
+import 'package:flutter/services.dart';
+
+class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+          primarySwatch: Colors.orange, primaryColor: AppColors.primary),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
+        "/insert_boleto": (context) => InsertBoletoPage()
+      },
+    );
+  }
+}
---
+++ lib/modules/barcode_scanner/barcode_scanner_status.dart
@@ -0,0 +1,34 @@
+class BarcodeScannerStatus {
+  final bool isCameraAvalable;
+  final String error;
+  final String barcode;
+  final bool stopScanner;
+
+  BarcodeScannerStatus({
+    this.isCameraAvalable = false,
+    this.error = "",
+    this.barcode = "",
+    this.stopScanner = false,
+  });
+
+  factory BarcodeScannerStatus.available() => BarcodeScannerStatus(
+        isCameraAvalable: true,
+        stopScanner: false,
+      );
+
+  factory BarcodeScannerStatus.error(String message) => BarcodeScannerStatus(
+        error: message,
+        stopScanner: true,
+      );
+
+  factory BarcodeScannerStatus.barcode(String barcode) => BarcodeScannerStatus(
+        barcode: barcode,
+        stopScanner: true,
+      );
+
+  bool get showCamera => isCameraAvalable && error.isEmpty;
+
+  bool get hasError => error.isNotEmpty;
+
+  bool get hasBarcode => barcode.isNotEmpty;
+}
